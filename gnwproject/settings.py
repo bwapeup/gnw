88596a02018aed88be3af58815eb8058adaedac3
bwapeup <bwapeup12345@gmail.com>
2018-03-30 17:36:39 +0800

    add static_root in settings

--- gnwproject/settings.py
+++ gnwproject/settings.py
@@ -133,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
 LOGIN_REDIRECT_URL = '/panel'  
 LOGOUT_REDIRECT_URL = '/panel'
 
