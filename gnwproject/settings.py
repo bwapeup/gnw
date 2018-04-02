cffd9303699cb9b8b72e8985b933780f6a5b0bfb
bwapeup <bwapeup12345@gmail.com>
2018-04-02 14:20:05 +0800

    add AWS Cloudfront settings

--- gnwproject/settings.py
+++ gnwproject/settings.py
@@ -131,25 +131,26 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 #STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
+
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'gnw/static'),
+#    os.path.join(BASE_DIR, 'sms/static'),
+#]
+
+AWS_LOCATION = 'static'
 #AWS_ACCESS_KEY_ID = config('AWS_KEY_ID')
 #AWS_SECRET_ACCESS_KEY = config('AWS_KEY')
-AWS_STORAGE_BUCKET_NAME = config('AWS_BUCKET_NAME')
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-AWS_LOCATION = 'static'
+#AWS_STORAGE_BUCKET_NAME = config('AWS_BUCKET_NAME')
+#AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+#AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400',}
+#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+AWS_CLOUDFRONT_DOMAIN = config('AWS_CLOUDFRONT_DOMAIN')
+STATIC_URL = 'https://%s/%s/' % (AWS_CLOUDFRONT_DOMAIN, AWS_LOCATION)
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'gnw/static'),
-    os.path.join(BASE_DIR, 'sms/static'),
-]
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 LOGIN_REDIRECT_URL = '/panel'  
